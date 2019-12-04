273f5b6d6bac771e1eecf3fa5a9c843a7f6b3109
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-12-04 02:12:04 -0500

    day 4, part 1 complete

--- 4/1-secure-container.js
+++ 4/1-secure-container.js
@@ -1,15 +1,11 @@
-// const min = 236491;
-const min = 300000;
-// const max = 713787;
-const max = 400000;
+const min = 236491;
+const max = 713787;
 
 const passwords = [];
 const doubleRegex = /([0-9])\1+/;
 
 const increasing = numStr => {
   const arr = numStr.split('');
-
-  if (Number(arr[0]) > Number(arr[1])) return false;
 
   for (let i = 1; i < arr.length; i++) {
     if (Number(arr[i - 1]) > Number(arr[i])) return false;
@@ -22,5 +18,5 @@ for (let i = min; i <= max; i++) {
   if (doubleRegex.test(iStr) && increasing(iStr)) passwords.push(i);
 }
 
-console.log('passwords', passwords);
+console.log('passwords', passwords.slice(passwords.length - 50));
 console.log('password #', passwords.length);
